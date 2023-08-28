1b4734778c94dc29234922cc2f18dc309bc4b017
Ryeo345 <philipruan345@gmail.com>
2023-08-27 23:36:48 -0400

    updated scripts and changed setup

--- server/db/conn.js
+++ server/db/conn.js
@@ -4,9 +4,6 @@ const config = {};
 if (process.env.QUIET) {
   config.logging = false;
 }
-const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
-  config
-);
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:code@localhost/acme_db', config);
 
 module.exports = conn;
